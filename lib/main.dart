8b31b5a7b05ceef1bf339ed4e464b0f48936f0eb
Umair Jibran <umairjibran7@gmail.com>
2020-08-21 10:05:17 +0500

    completed the floating action button's menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:hawk_fab_menu/hawk_fab_menu.dart';
 import './screens/home.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   final String userName = "Umair Jibran";
+  final List<HawkFabMenuItem> hawkFABS = [
+    HawkFabMenuItem(
+      label: 'Arrived at Port',
+      ontap: () {},
+      icon: Icon(Icons.archive),
+      color: Colors.orange[600],
+    ),
+    HawkFabMenuItem(
+      label: 'De-Stuff at Port',
+      ontap: () {},
+      icon: Icon(Icons.local_convenience_store),
+      color: Colors.blue,
+    ),
+    HawkFabMenuItem(
+      label: 'Dispach from Port',
+      ontap: () {},
+      icon: Icon(Icons.local_shipping),
+      color: Colors.green,
+    ),
+    HawkFabMenuItem(
+      label: 'Arrived at FF Location',
+      ontap: () {},
+      icon: Icon(
+        Icons.shopping_basket,
+      ),
+      color: Colors.orange[300],
+    ),
+    HawkFabMenuItem(
+      label: 'Containers Offloaded',
+      ontap: () {},
+      icon: Icon(Icons.file_download),
+      color: Colors.teal,
+    ),
+    HawkFabMenuItem(
+      label: 'Returned Containers',
+      ontap: () {},
+      icon: Icon(Icons.local_shipping),
+      color: Colors.purple,
+    ),
+  ].reversed.toList();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(Icons.add),
-          backgroundColor: Colors.orange,
-        ),
-        floatingActionButtonLocation:
-            FloatingActionButtonLocation.miniEndDocked,
         backgroundColor: Colors.white,
         appBar: AppBar(
           title: Text(
@@ -50,7 +84,14 @@ class MyApp extends StatelessWidget {
             )
           ],
         ),
-        body: HomeScreen(),
+        body: HawkFabMenu(
+          blur: 1,
+          icon: AnimatedIcons.add_event,
+          iconColor: Colors.white,
+          fabColor: Colors.orange,
+          items: hawkFABS,
+          body: HomeScreen(),
+        ),
       ),
     );
   }
